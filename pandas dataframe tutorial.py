0b945801ac9b5047ee1d72f7c4b1681e37bd61d9
TanvirAlamSyed1 <117751149+TanvirAlamSyed1@users.noreply.github.com>
2022-11-08 19:22:51 +0000

    Add files via upload

---
+++ pandas dataframe tutorial.py
@@ -0,0 +1,34 @@
+# the work has come from- https://colab.research.google.com/github/google/eng-edu/blob/main/ml/cc/exercises/pandas_dataframe_ultraquick_tutorial.ipynb?utm_source=mlcc&utm_campaign=colab-external&utm_medium=referral&utm_content=pandas_tf2-colab&hl=en#scrollTo=RIO91Fu65s6k
+
+import numpy as np
+import pandas as pd
+
+# Create and populate a 5x2 NumPy array.
+my_data = np.array([[0, 3], [10, 7], [20, 9], [30, 14], [40, 15]])
+
+# Create a Python list that holds the names of the two columns.
+my_column_names = ['temperature', 'activity']
+
+# Create a DataFrame.
+my_dataframe = pd.DataFrame(data=my_data, columns=my_column_names)
+
+# Print the entire DataFrame
+print(my_dataframe)
+
+# Create a new column named adjusted.
+my_dataframe["adjusted"] = my_dataframe["activity"] + 2
+
+# Print the entire DataFrame
+print(my_dataframe)
+
+print("Rows #0, #1, and #2:")
+print(my_dataframe.head(3), '\n')
+
+print("Row #2:")
+print(my_dataframe.iloc[[2]], '\n')
+
+print("Rows #1, #2, and #3:")
+print(my_dataframe[1:4], '\n')
+
+print("Column 'temperature':")
+print(my_dataframe['temperature'])
